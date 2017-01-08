e7bba6b968f1b04eca07829d2d3af2ade3d77ca5
abraaoalves <abraao.teodosio@gmail.com>
2017-01-08 20:31:18 -0300

    feat(definition): add and ref

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+
+
+export as namespace configure;
+
+declare function configure(): () =>void;
+declare namespace configure{
+
+  //TODO: make feathers-hooks-common definition
+  /**
+   * export removeQuery
+   * export pluckQuery
+   * export lowerCase
+   * export remove
+   * export pluck
+   * export disable
+   * export populate
+   * export removeField
+   */
+}
