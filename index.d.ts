f857db40857ac539f3a51fdc1aecd44d5312e9d6
abraaoalves <abraao.teodosio@gmail.com>
2017-01-16 11:45:22 -0300

    fix(definition): remove default export
    babel plugin "add-module-exports" generate a module.exports and remove default export

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,4 @@
 
 
-export as namespace configure;
-
-export default function configure(): () =>void;
-//declare namespace configure{
-
-  //TODO: make feathers-hooks-common definition
-  /**
-   * export removeQuery
-   * export pluckQuery
-   * export lowerCase
-   * export remove
-   * export pluck
-   * export disable
-   * export populate
-   * export removeField
-   */
-//}
+declare function configure(): () =>void;
+export = configure;
