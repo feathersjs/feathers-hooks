d26441c15560a40e5e3b338f3f4b9f3078dbdc4d
Richard Michael Coo <augustuslogos@gmail.com>
2017-03-02 03:44:00 +0800

    Place custom types and interfaces under namespace

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,37 @@
 import * as feathers from 'feathers';
 
-interface Hook {
-  <T>(hook: HookProps<T>): Promise<any> | void;
-}
-
-interface HookProps<T> {
-  method?: string;
-  type: 'before' | 'after';
-  params?: any;
-  data?: T;
-  result?: T;
-  app?: feathers.Application;
-}
-
-interface HookMap {
-  all?: Hook | Hook[];
-  find?: Hook | Hook[];
-  get?: Hook | Hook[];
-  create?: Hook | Hook[];
-  update?: Hook | Hook[];
-  patch?: Hook | Hook[];
-  remove?: Hook | Hook[];
-}
-
-type HookType = HookMap | ((hooks: HookMap) => void);
-
 declare module 'feathers' {
   interface Service<T> {
-    before: HookType;
-    after: HookType;
+    before(hooks: hooks.HookMap);
+    after(hooks: hooks.HookMap);
   }
 }
 
 declare function hooks(): () => void;
 
-declare namespace hooks{}
+declare namespace hooks {
+  interface Hook {
+    <T>(hook: HookProps<T>): Promise<any> | void;
+  }
+
+  interface HookProps<T> {
+    method?: string;
+    type: 'before' | 'after';
+    params?: any;
+    data?: T;
+    result?: T;
+    app?: feathers.Application;
+  }
+
+  interface HookMap {
+    all?: Hook | Hook[];
+    find?: Hook | Hook[];
+    get?: Hook | Hook[];
+    create?: Hook | Hook[];
+    update?: Hook | Hook[];
+    patch?: Hook | Hook[];
+    remove?: Hook | Hook[];
+  }
+}
+
 export = hooks;
