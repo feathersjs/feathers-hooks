87545e2d051c8e708ce68730108fea3e7f400cca
Richard Michael Coo <augustuslogos@gmail.com>
2017-03-02 01:30:52 +0800

    Augment Service<T> typedef with before() and after() methods

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,38 @@
+import * as feathers from 'feathers';
 
+interface Hook {
+  <T>(hook: HookProps<T>): Promise<any> | void;
+}
 
-declare function configure(): () =>void;
-declare namespace configure{}
-export = configure;
+interface HookProps<T> {
+  method?: string;
+  type: 'before' | 'after';
+  params?: any;
+  data?: T;
+  result?: T;
+  app?: feathers.Application;
+}
+
+interface HookMap {
+  all?: Hook | Hook[];
+  find?: Hook | Hook[];
+  get?: Hook | Hook[];
+  create?: Hook | Hook[];
+  update?: Hook | Hook[];
+  patch?: Hook | Hook[];
+  remove?: Hook | Hook[];
+}
+
+type HookType = HookMap | ((hooks: HookMap) => void);
+
+declare module 'feathers' {
+  interface Service<T> {
+    before: HookType;
+    after: HookType;
+  }
+}
+
+declare function hooks(): () => void;
+
+declare namespace hooks{}
+export = hooks;
